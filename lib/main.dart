6fcd1229ef06ec5ae80ca6165d2ffa5e7f5946d0
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2026-01-06 18:40:25 +0600

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home:  MyHomePage(title: 'Node'),
+    );
+  }
+}
